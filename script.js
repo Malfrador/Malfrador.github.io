032737d64fb61752dc3286aa852fb083def8d348
Malfrador <malfrador@gmail.com>
2024-06-11 06:50:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,164 @@
+document.addEventListener('DOMContentLoaded', () => {
+  class SimpleGallery {
+    constructor(images, descriptions, containerId) {
+      this.images = images;
+      this.descriptions = descriptions;
+      this.currentIndex = 0;
+      this.containerId = containerId;
+      this.initGallery();
+    }
+
+    initGallery() {
+      const container = document.getElementById(this.containerId);
+      container.innerHTML = `
+      <div class="gallery">
+        <div class="galleryImgContainer">
+            <button class="prevBtn">&#10094;</button>
+            <img class="galleryImg" src="${this.images[0]}" alt="Gallery Image">
+            <div class="imageDescription">${this.descriptions[0]}</div>
+            <button class="nextBtn">&#10095;</button>
+        </div>
+    </div>
+    `;
+
+      const prevBtn = container.querySelector('.prevBtn');
+      const nextBtn = container.querySelector('.nextBtn');
+      prevBtn.addEventListener('click', () => this.prevImage());
+      nextBtn.addEventListener('click', () => this.nextImage());
+    }
+
+    showImage(index) {
+      const container = document.getElementById(this.containerId);
+      container.querySelector('.galleryImg').src = this.images[index];
+      container.querySelector('.imageDescription').innerText = this.descriptions[index];
+    }
+
+    nextImage() {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      this.showImage(this.currentIndex);
+    }
+
+    prevImage() {
+      this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+      this.showImage(this.currentIndex);
+    }
+  }
+
+  const gallery1 = new SimpleGallery(
+    ['images/hunt/cover.jpg', 'images/hunt/mechanics.png'],
+    ['A scene from the prototype level', 
+    'A variety of mechanics, including climbing, dragging of bodies, destructible walls, items and respawning.'],
+    'gallery1'
+  );
+
+  const gallery2 = new SimpleGallery(
+    ['images/gygo/cover.png', 'images/gygo/programming.png', 'images/gygo/dialogues.png', 'images/gygo/gamedesign.png', 'images/gygo/pdf.png'],
+    ['The main screen of the game. Here the player has to complete all rooms, for a total of 7 tasks.', 
+    'The programming task. Inspired by visual scripting like Blueprints, the player has to get the character on the right to move.', 
+    'The assessment also features a dialogue system, to immerse the player into the setting.', 
+    'The game design task. The player has to find applicable player types for a given game.', 
+    'A PDF is generated at the end of the assessment. It contains several metrics that can be used to judge the player\'s performance.'],
+    'gallery2'
+  );
+
+  const gallery3 = new SimpleGallery(
+    ['images/papyrus/graph.png', 'images/papyrus/rpg.png', 'images/papyrus/lotsofnpcs.png'],
+    ['Schematic verview of the Spellbook skill system','Elements of the RPG systems, including UI, items, item upgrades, passive traits and enemy AI', 
+    'A large number of aggressive NPCs. The server is able to handle several thousands of them at 20hz'],
+    'gallery3'
+  );
+  const gallery4 = new SimpleGallery(
+    ['images/qxl/cover.png', 'images/qxl/example.png', 'images/qxl/dialogue.png', 'images/qxl/minimap.png'],
+    ['One of the main cities. They act as a hub where the player can interact with NPCs, buy and sell items and complete quests.', 
+    'An example of a simple QXL quest definition, written in YAML', 
+    'The dialogue system. Minecraft only has a single chat channel and no ability to delete messages, so a few tricks were neccessary to preserve chat history', 
+    'The minimap, available without requiring client-side mods.'],
+    'gallery4'
+  );
+});
+
+
+function getRandomColor() {
+  const getRandomValue = () => Math.floor(Math.random() * 256);
+  const getContrastYIQ = (r, g, b) => {
+    const yiq = ((r*299)+(g*587)+(b*114)) / 1000;
+    return (yiq >= 128) ? 'black' : 'white';
+  };
+  const r = getRandomValue();
+  const g = getRandomValue();
+  const b = getRandomValue();
+  const backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  const textColor = getContrastYIQ(r, g, b);
+
+  return { backgroundColor, textColor };
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const techLabels = document.querySelectorAll('.technology-label');
+  techLabels.forEach(label => {
+    const colors = getRandomColor();
+    label.style.backgroundColor = colors.backgroundColor;
+    label.style.color = colors.textColor;
+  });
+    if (window.location.hash) {
+      const projectName = window.location.hash.substring(1);
+      togglePopup(projectName);
+  }
+  const projects = document.querySelectorAll('.project');
+  projects.forEach(project => {
+    project.addEventListener('mousedown', (e) => {
+      if (e.button === 1) { // Middle-click
+        const popup = project.querySelector('.popup');
+        const projectName = popup.getAttribute('data-project');
+        const newTabUrl = `${window.location.origin}${window.location.pathname}#${projectName}`;
+        window.open(newTabUrl, '_blank');
+      }
+    });
+  });
+});
+ 
+const friendlyNames = {
+  'hunt': 'Mythical Hunt',
+  'gygo': 'Get your Game on',
+  'qxl': 'QuestsXL',
+  'papyrus': 'Papyrus',
+  'obession': 'Obsession',
+};
+function togglePopup(projectName, event) {
+  const popup = document.querySelector('.popup[data-project="' + projectName + '"]');
+  const popupContent = popup.querySelector('.popup-content');
+  const closePopup = function(event) {
+      if (!popup.contains(event.target)) {
+          popup.style.display = 'none';
+          history.pushState(null, '', window.location.pathname); // Reset URL
+          document.removeEventListener('mousedown', closePopup);
+          document.title = 'Malte Schümann';
+      }
+  };
+  if (popup && popupContent) {
+      const closeButton = popup.querySelector('.close-btn');
+      if (closeButton && !closeButton.dataset.listenerAdded) {
+          closeButton.addEventListener('click', (e) => {
+              e.stopPropagation(); 
+              popup.style.display = 'none';
+              history.pushState(null, '', window.location.pathname); // Reset URL
+          });
+          closeButton.dataset.listenerAdded = 'true';
+      }
+      if (popup.style.display === 'none' || popup.style.display === '') {
+          popup.style.display = 'block';
+          popupContent.style.display = 'block';
+          history.pushState(null, '', `#${projectName}`); // Update URL
+          document.addEventListener('mousedown', closePopup);
+          const name = friendlyNames[projectName] || projectName;
+          document.title = name + ' - Malte Schümann';
+      } else {
+          if (event && !popupContent.contains(event.target)) {
+              popup.style.display = 'none';
+              history.pushState(null, '', window.location.pathname); // Reset URL
+          }
+      }
+  }
+}
+
+window.togglePopup = togglePopup;
